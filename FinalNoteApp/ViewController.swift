4772d906fb88779c86315ddd43c0aa94525f2690
Bryce Hawkins <brycehawkins.dev@gmail.com>
2022-12-30 12:12:40 -0500

    Initial Commit

---
+++ FinalNoteApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinalNoteApp
+//
+//  Created by Bryce Hawkins on 12/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
